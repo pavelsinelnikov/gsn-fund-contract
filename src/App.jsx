2f30fc0f155dfb025d6803b5ddbf4116d458ce0a
Pavel Sinelnikov <pavel.sinelnikov@outlook.com>
2021-03-05 13:12:34 -0500

    Added stling changes and error handling

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,11 @@ import Web3 from 'web3';
 import hubAbi from './hubAbi.json';
 import defaultHubs from './defaultHubs.json';
 
-const defaultNetwork = 'mainnet';
-const portisDappId = '211b48db-e8cc-4b68-82ad-bf781727ea9e';
+const defaultNetwork = 'ropsten';
+const portisDappId = '0b18bb5e-1019-4f19-bf23-fc995a38dcfb';
 
 let portis = new Portis(portisDappId, defaultNetwork);
 let web3 = new Web3(portis.provider);
-let hubContract = new web3.eth.Contract(hubAbi, defaultHubs[defaultNetwork]);
 
 export default class App extends Component {
 	constructor(props) {
@@ -22,16 +21,39 @@ export default class App extends Component {
 			network: defaultNetwork,
 			hubAddress: defaultHubs[defaultNetwork],
 			contractAddress: '',
-			amountInEther: 1,
+			amountInEther: 0.01,
 			balance: 0,
 			loading: false,
+			message: '',
 		};
 	}
+
+	validAddresses = () => {
+		let message = '';
+		if (!web3.utils.isAddress(this.state.hubAddress)) {
+			message = 'Error: Incorrect address for the Hub Contract Address';
+		}
+
+		if (!web3.utils.isAddress(this.state.contractAddress)) {
+			if (message) {
+				message += ' and for Your Contract';
+			} else {
+				message = 'Error: Incorrect address for Your Contract';
+			}
+		}
+
+		this.setState({ message });
+
+		if (message) {
+			return false;
+		}
+
+		return true;
+	};
 
 	handleNetworkChange = (e) => {
 		portis = new Portis(portisDappId, e.target.value);
 		web3 = new Web3(portis.provider);
-		hubContract = new web3.eth.Contract(hubAbi, defaultHubs[e.target.value]);
 
 		this.setState({
 			network: e.target.value,
@@ -40,26 +62,46 @@ export default class App extends Component {
 	};
 
 	getBalance = async () => {
-		const balance = await hubContract.methods
-			.balanceOf(this.state.contractAddress)
-			.call();
-		this.setState({ balance });
+		if (this.validAddresses()) {
+			const hubContract = new web3.eth.Contract(hubAbi, this.state.hubAddress);
+			const balance = web3.utils.fromWei(
+				await hubContract.methods.balanceOf(this.state.contractAddress).call()
+			);
 
-		console.log(balance);
+			this.setState({ balance });
+		}
 	};
 
 	handleSubmit = async () => {
 		this.setState({ loading: true });
 		try {
-			const accounts = await web3.currentProvider.enable();
-			await hubContract.methods.depositFor(this.state.contractAddress).send({
-				from: accounts[0],
-				value: this.state.amountInEther * 10 ** 18,
-			});
-
-			console.log(`🚀 Your balance in the Hub is: ${balance / 10 ** 18} ETH`);
+			if (this.validAddresses()) {
+				const hubContract = new web3.eth.Contract(
+					hubAbi,
+					this.state.hubAddress
+				);
+				const accounts = await web3.currentProvider.enable();
+				await hubContract.methods
+					.depositFor(this.state.contractAddress)
+					.send({
+						from: accounts[0],
+						value: web3.utils.toWei(
+							this.state.amountInEther.toString(),
+							'ether'
+						),
+					})
+					.on('transactionHash', (hash) => {
+						this.setState({
+							message: 'Transaction Hash: ' + hash,
+						});
+					});
+			}
 		} catch (error) {
-			console.error(error);
+			this.setState({
+				message:
+					'Error: Something went wrong with submitting, check the console for more details',
+			});
+			console.log(error);
 		} finally {
 			this.setState({ loading: false });
 		}
@@ -72,6 +114,16 @@ export default class App extends Component {
 	render() {
 		return (
 			<div className="App container">
+				<header className="row">
+					<div className="six columns">
+						<h1>Gas Station Funding</h1>
+					</div>
+					<div className="six columns smallTop">
+						<p>
+							<b>{this.state.message}</b>
+						</p>
+					</div>
+				</header>
 				<form>
 					<div className="row">
 						<div className="six columns">
@@ -98,7 +150,9 @@ export default class App extends Component {
 							>
 								<option value="mainnet">Mainnet</option>
 								<option value="rinkeby">Rinkeby</option>
-								<option value="ropsten">Ropsten</option>
+								<option selected value="ropsten">
+									Ropsten
+								</option>
 								<option value="kovan">Kovan</option>
 								<option value="goerli">Goerli</option>
 								<option value="xdai">xDai</option>
@@ -130,8 +184,9 @@ export default class App extends Component {
 								Get Balance
 							</button>
 						</div>
-						<div className="three columns smallTop">
-							{this.state.balance} ETH
+						<div className="three columns">
+							<label>Contract Balance</label>
+							<p>{this.state.balance} ETH</p>
 						</div>
 					</div>
 
@@ -142,14 +197,14 @@ export default class App extends Component {
 								name="amountInEther"
 								className="u-full-width"
 								value={this.state.amountInEther}
-								placeholder="1"
+								placeholder="Amount"
 								type="text"
 								onChange={this.handleChange}
 								id="amountInEtherInput"
 							/>
 						</div>
 						<div className="six columns smallTop">
-							{!this.state.loading && (
+							{!this.state.loading ? (
 								<button
 									type="button"
 									className="u-full-width"
@@ -157,11 +212,24 @@ export default class App extends Component {
 								>
 									Fund
 								</button>
+							) : (
+								<p>
+									<b>Processing Transaction...</b>
+								</p>
 							)}
-							{this.state.loading}
 						</div>
 					</div>
 				</form>
+				<footer>
+					<p>
+						Credit to <a href="https://github.com/radotzki">Itay Radotzki</a>{' '}
+						for creating the{' '}
+						<a href="https://codesandbox.io/s/00jq8xn55v">
+							Fund Contract in Relay Hub
+						</a>{' '}
+						template
+					</p>
+				</footer>
 			</div>
 		);
 	}
